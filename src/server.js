84c96a84eaaab0e811bf532af78378210b363e43
조현일 <himzei@gmail.com>
2021-06-14 19:09:13 +0900

    position

--- src/server.js
+++ src/server.js
@@ -23,17 +23,10 @@ const apollo = new ApolloServer({
 const app = express();
 
 app.use(logger("tiny"));
-app.use(cors());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept Authorization"
-  );
-  next();
-});
+
 apollo.applyMiddleware({ app });
 app.use("/static", express.static("uploads"));
+app.use(cors());
 
 app.listen({ port: PORT }, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
